06c9d180b8a790fa51be16dd7c3f4ea5dc8ee6d7
ali <3alimirhashemi@gmail.com>
2024-06-29 19:43:40 +0330

    session 9

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./style.css"
 
 class App extends React.Component{
   render(){
     return(
-      <h1>hi guys</h1>
+      <h1>Hi Guys</h1>
     )
   }
 }
 
 class Msg extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      time :new Date().toLocaleTimeString()
+
+    }
+  }
   render(){
+    setInterval(()=>{
+      this.setState({
+        time :new Date().toLocaleTimeString()
+      })
+    },1000)
     return(
-        <h2>
-          it is time:{new Date().toLocaleTimeString()};
+        <h2 className='timer'>
+          it is time: {this.state.time}
         </h2>
 
   )
@@ -24,7 +37,7 @@ class Msg extends React.Component{
 class Timer extends React.Component{
   render(){
     return(
-      <div>
+      <div className='main'>
         <App/>
         <Msg/>
       </div>
@@ -32,11 +45,6 @@ class Timer extends React.Component{
   }
 }
 
-const tick = ()=>{
+
   ReactDOM.createRoot(document.getElementById('root')).render(<Timer/>);
 
-}
-
-setInterval(()=>{
-  tick();
-},1000)

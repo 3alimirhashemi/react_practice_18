02649a0b404729e025dc28f45527e4a4de7cf52e
ali <3alimirhashemi@gmail.com>
2024-06-29 20:36:31 +0330

    session 11

--- src/msg.js
+++ src/msg.js
@@ -10,12 +10,15 @@ class Msg extends React.Component{
   
       }
     }
-    render(){
+    componentDidMount(){
       setInterval(()=>{
         this.setState({
           time :new Date().toLocaleTimeString()
         })
       },1000)
+    }
+
+    render(){
       return(
           <h2 className='timer'>
             it is time: {this.state.time}

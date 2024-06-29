ee73d4cb8a5150b7635a6d32ffe2b87113f0cab9
ali <3alimirhashemi@gmail.com>
2024-06-29 18:41:31 +0330

    session 7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Timer = ()=>{
+    const time =(
+      <div>
+        <h1>hi guys</h1>
+        <h2>
+          it is time:{new Date().toLocaleTimeString()};
+        </h2>
+      </div>
+    )
+    ReactDOM.createRoot(document.getElementById('root')).render(time);
+}
+
+setInterval(()=>{
+  Timer();
+},1000)

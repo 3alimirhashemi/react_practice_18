50ea01d373ee09bf33ba4cef9c301cb5dc0823e1
ali <3alimirhashemi@gmail.com>
2024-07-01 21:05:51 +0330

    session 13

--- src/countdown.js
+++ src/countdown.js
@@ -2,42 +2,72 @@ import ReactDOM from 'react-dom/client';
 import "./style.css"
 import React from "react";
 
+
+var interval;
 class CountDown extends React.Component {
   constructor() {
     super();
-    this.state = { seconds: 20 };
+    this.state = { 
+      seconds: 0,
+      minutes: 0,
+      hour: 0,
+      isStart:false,
+    };
   }
-
-  tick() {
-    this.setState(state => ({
-      seconds: state.seconds - 1
-    }));
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  componentDidUpdate(){
-    if (this.state.seconds == "0"){
-      clearInterval(this.interval);
-
+  startInterval = () => {
+    if(this.state.isStart == false){
+      interval = setInterval(()=>{
+        this.setState({
+          seconds : this.state.seconds +1
+        })
+        if(this.state.seconds == 60){
+          this.setState({
+            seconds: 1, 
+            minutes: this.state.minutes + 1,
+          })
+        }
+        if(this.state.minutes == 60){
+          this.setState({
+            minutes: 1,
+            hour: this.state.hour + 1,
+          })
+        } 
+      }, 1000)
     }
+
   }
+
+  stopInterval = () =>{
+    this.setState({
+      isStart:false
+    })
+    clearInterval(interval);
+  }
+
+  resetInterval = () => {
+    this.stopInterval();
+    this.setState({
+      hour:0,
+      minutes:0,
+      seconds:0,
+    })
+  }
+
   
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
 
   render() {
+    let h = this.state.hour
+    let m = this.state.minutes
+    let s = this.state.seconds
     return (
       <div>
-        <form>
-
-        </form>
-        Seconds: {this.state.seconds}
-        
+        {`${h>9? h:"0"+h} : ${m>9? m:"0"+m} : ${s>9? s:"0"+s}`}
+        <div>
+        <button onClick={this.startInterval}>start</button>
+        <button onClick={this.stopInterval}>stop</button>
+        <button onClick={this.resetInterval}>reset</button>
+        </div>
       </div>
      
     );

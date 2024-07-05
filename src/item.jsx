3137a918e4871a49de9153cbe33dc4c10cad61d2
ali <3alimirhashemi@gmail.com>
2024-07-05 11:32:11 +0330

    session 19

--- src/item.jsx
+++ src/item.jsx
@@ -1,6 +1,14 @@
+import { useContext } from "react"
+import testcontext from "./context"
+
 const Item = (props)=>{
+    const context = useContext(testcontext)
+    const handleDelete = (e)=>{
+        context.setTime(context.time.filter(t=>t != e.target.innerHTML))
+
+    }
     return(
-        <div>
+        <div onClick={handleDelete}>
             {props.children}
         </div>
     )
